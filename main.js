101e785cad226930c0f184bae4280862cb2fa0fc
Huyifan19 <Yifan.Hu19@student.xjtlu.edu.cn>
2023-05-11 21:35:13 +0800

    Signed-off-by: Huyifan19 <Yifan.Hu19@student.xjtlu.edu.cn>

--- main.js
+++ main.js
@@ -58,30 +58,32 @@ function getHotelsAndWeather() {
   getWeather(city);
 
   // Fetch hotels
-  getHotels(city);
+  
+  getHotels(city, selectedHoliday);
+
+  getWeatherdata(city, selectedHoliday)
 }
 // 获取酒店的函数
-function getHotels() {
-  const city = document.getElementById("city").value;
-  const url = `https://hotels4.p.rapidapi.com/locations/search?query=${city}&locale=en_US`;
-  const settings = {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': '84b4e004d4msh6eabc7a0120cf44p1d9ce3jsn5502a62f81e9',
-      'x-rapidapi-host': 'hotels4.p.rapidapi.com',
-    },
-  };
+function getHotels(city, selectedHoliday) {
+    const url = `https://hotels4.p.rapidapi.com/locations/search?query=${city}&checkin_date=${selectedHoliday}&checkout_date=${selectedHoliday}&locale=en_US`;
+    const settings = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'bf20b74b90mshb7b0688b1eb9222p1e6e7bjsnc43014e44aaa',
+            'x-rapidapi-host': 'hotels4.p.rapidapi.com',
+        },
+    };
 
-  fetch(url, settings)
-    .then(response => response.json())
-    .then(data => {
-        const hotelList = document.getElementById("hotelList");
-        hotelList.innerHTML = "Hotel List:<br>";
-        data.suggestions[1].entities.forEach(hotel => {
-          hotelList.innerHTML += `${hotel.name}<br>`;
-        });
-      })
-      .catch(error => console.error(error));
+    fetch(url, settings)
+        .then(response => response.json())
+        .then(data => {
+            const hotelList = document.getElementById("hotelList");
+            hotelList.innerHTML = "Hotel List:<br>";
+            data.suggestions[1].entities.forEach(hotel => {
+              hotelList.innerHTML += `${hotel.name}<br>`;
+            });
+          })
+          .catch(error => console.error(error));
     }
     //获取天气
     function getWeather() {
@@ -107,4 +109,28 @@ function getHotels() {
           })
           .catch(error => console.error(error));
   }
+  function getWeatherdata(city, selectedHoliday) {
+    const lat = 35.6895; // Latitude of the city
+    const lon = 139.6917; // Longitude of the city
+    const dt = new Date(selectedHoliday).getTime() / 1000; // Convert the selected holiday to UNIX timestamp
+    const apiKey = 'e5eb20914f993f76ee41d0eabe52ab75'; // Replace with your API key
   
+    const url = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${dt}&appid=${apiKey}`;
+  
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const weatherList = document.getElementById("weatherList");
+            const description = data.current.weather[0].description;
+            const temp = Math.round(data.current.temp * 100) / 100;
+  
+            weatherList.innerHTML = `Weather on ${selectedHoliday}: ${description}<br>`;
+            weatherList.innerHTML += `Temperature: ${temp} °C`;
+        })
+        .catch(error => console.error(error));
+}
